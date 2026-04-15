cc3e3ceacd61475badf5437361a3034eadc87c6a
hariomtiwari80 <hariomtiwari.cse@gmail.com>
2026-04-15 17:17:08 +0530

    Update File System.js

--- File System.js
+++ File System.js
@@ -3,7 +3,7 @@ const fs=require('fs')
 // writing data to file
 fs.writeFileSync("dummy.txt","Hello I am Hari !!!")                //Sync
 fs.writeFile("dummy1.txt","Hello I am ...",(err)=>{                //Async
-    console.error(err);
+    if(err) console.error(err);
 })
 
 // Reading data from file
@@ -15,7 +15,41 @@ fs.readFile("dummy1.txt","utf8",(err,res)=>{                       //Async
 })
 
 //Creating a directory
-fs.mkdirSync("myfolder")                                            //Sync
-fs.mkdir('Folder2',(err)=>{                                         //Async
+try{                                                               //Sync
+    fs.mkdirSync("myfolder")
+}catch(err){
+    if(err) console.error(err)
+}
+fs.mkdir('Folder2',(err)=>{                                        //Async
     if(err) console.error(err);
-})                                                
+})          
+
+//Deleting a directory
+try{                                                               //Sync
+    fs.rmSync("myfolder",{ recursive: true, force: true })
+}catch(err){
+    console.error(err);
+}
+fs.rm("Folder2",{ recursive: true, force: true },(err)=>{          //Async
+    if(err) console.error(err)
+})
+
+//Deleting a file
+try{                                                               //Sync
+    fs.unlinkSync('dummy.txt')
+}catch(err){
+    if(err) console.log(err)
+}
+fs.unlink('dummy1.txt',(err)=>{                                    //Async
+    if(err) console.error(err);
+})
+
+//Rename a file/folder
+try{                                                                //Sync
+    fs.renameSync('dummy.txt','hari.txt');
+}catch(err){
+    if(err) console.log(err);
+}
+fs.rename('hari.txt','dummy.txt',(err)=>{                           //Async
+    if(err) console.error(err);
+})
