6a190032264392a368897f51943524d07f03d105
hariomtiwari80 <hariomtiwari.cse@gmail.com>
2026-05-18 11:48:12 +0530

    Add files via upload

---
+++ TASK MANAGER/src/controller/auth.controller.js
@@ -0,0 +1,24 @@
+export const login = (req, res) => {
+    const { username } = req.body;
+
+    if (!username) {
+        return res.status(400).json({ error: "Username is required" });
+    }
+
+    req.session.user = { username };
+
+    res.cookie("username", username, { httpOnly: true, maxAge: 1000 * 60 * 60 * 24 });
+
+    res.json({ message: "Login Successful", username });
+};
+
+
+export const logout = (req , res)=>{
+    res.clearCookie("username");
+    res.session.destroy((err)=>{
+        if(err){
+            return res.status(500).json({error:"Error logging out"})
+        }
+        res.json({message:"Logout Successful"})
+    })
+}
